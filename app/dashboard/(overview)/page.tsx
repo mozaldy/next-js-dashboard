130d5ec1ca486e2d1739a68a81b372ff1a69dbac
mozaldy <moxaldy@gmail.com>
2024-07-08 22:56:53 +0700

    Add metadata

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -2,18 +2,27 @@ import CardWrapper from '@/app/ui/dashboard/cards';
 import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitana } from '@/app/ui/fonts';
-import { fetchLatestInvoices, fetchCardData } from '@/app/lib/data';
+// import { fetchLatestInvoices, fetchCardData } from '@/app/lib/data';
 import { Suspense } from 'react';
-import { RevenueChartSkeleton, LatestInvoicesSkeleton, CardsSkeleton } from '@/app/ui/skeletons';
- 
+import {
+  RevenueChartSkeleton,
+  LatestInvoicesSkeleton,
+  CardsSkeleton,
+} from '@/app/ui/skeletons';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Home',
+};
+
 export default async function Page() {
-  const latestInvoices = await fetchLatestInvoices();
-  const {
-    numberOfCustomers, 
-    numberOfInvoices, 
-    totalPaidInvoices, 
-    totalPendingInvoices
-  } = await fetchCardData();
+  // const latestInvoices = await fetchLatestInvoices();
+  // const {
+  //   numberOfCustomers,
+  //   numberOfInvoices,
+  //   totalPaidInvoices,
+  //   totalPendingInvoices
+  // } = await fetchCardData();
   return (
     <main>
       <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
@@ -28,15 +37,15 @@ export default async function Page() {
         {/*   value={numberOfCustomers} */}
         {/*   type="customers" */}
         {/* /> */}
-        <Suspense fallback={<CardsSkeleton/>}>
+        <Suspense fallback={<CardsSkeleton />}>
           <CardWrapper />
         </Suspense>
       </div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
-        <Suspense fallback={<RevenueChartSkeleton/>}>
+        <Suspense fallback={<RevenueChartSkeleton />}>
           <RevenueChart />
         </Suspense>
-        <Suspense fallback={<LatestInvoicesSkeleton/>}>
+        <Suspense fallback={<LatestInvoicesSkeleton />}>
           <LatestInvoices />
         </Suspense>
       </div>
--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -1,36 +1,40 @@
-import Form from "@/app/ui/invoices/edit-form";
-import Breadcrumbs from "@/app/ui/invoices/breadcrumbs";
-import { fetchInvoiceById, fetchCustomers } from "@/app/lib/data";
-import { notFound } from "next/navigation";
+import Form from '@/app/ui/invoices/edit-form';
+import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
+import { fetchInvoiceById, fetchCustomers } from '@/app/lib/data';
+import { notFound } from 'next/navigation';
+import { Metadata } from 'next';
 
-export default async function Page({ params }: { params: { id: string } }){
+export const metadata: Metadata = {
+  title: 'Edit Invoices',
+};
+
+export default async function Page({ params }: { params: { id: string } }) {
   const id = params.id;
   const [invoice, customers] = await Promise.all([
-  fetchInvoiceById(id), 
-  fetchCustomers(),
-]);
+    fetchInvoiceById(id),
+    fetchCustomers(),
+  ]);
 
-  if(!invoice){
+  if (!invoice) {
     notFound();
   }
 
   return (
     <main>
-      <Breadcrumbs breadcrumbs={[
-        {
-          label: 'Invoices',
-          href: '/dashboard/invoices',
-        },
-        {
-          label: 'Edit Invoices',
-          href: `/dashboard/invoices/${id}/edit`,
-          active: true,
-        }
-      ]} />
-      <Form 
-        customers={customers}
-        invoice={invoice}
+      <Breadcrumbs
+        breadcrumbs={[
+          {
+            label: 'Invoices',
+            href: '/dashboard/invoices',
+          },
+          {
+            label: 'Edit Invoices',
+            href: `/dashboard/invoices/${id}/edit`,
+            active: true,
+          },
+        ]}
       />
+      <Form customers={customers} invoice={invoice} />
     </main>
   );
 }
